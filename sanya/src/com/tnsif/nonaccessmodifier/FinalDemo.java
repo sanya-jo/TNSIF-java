b0f3f08413f5d4128331d86413045b216f1b3cd2
sanya <sanya@DESKTOP-DKK5DKT>
2025-11-20 22:05:45 +0530

    non accessmodifier

---
+++ sanya/src/com/tnsif/nonaccessmodifier/FinalDemo.java
@@ -0,0 +1,24 @@
+package com.tnsif.nonaccessmodifier;
+public class FinalDemo {
+		final int Max = 100;
+		
+		 void display() {
+			System.out.println("Show the value of max:  "+Max);
+		}
+
+		public static void main(String[] args) {
+			FinalDemo fd = new FinalDemo();
+		//	fd.Max = 300;
+			fd.display();
+			
+			
+		}
+}
+
+class Child extends FinalDemo{
+	
+      void display() {
+		System.out.println("Show the value of max:  "+Max);
+	}
+}
+
---
+++ sanya/src/com/tnsif/nonaccessmodifier/StaticDemo.java
@@ -0,0 +1,30 @@
+package com.tnsif.nonaccessmodifier;
+
+public class StaticDemo {
+	//static variable
+		static int count;
+		
+		//static block
+		static {
+			count = 20;
+			
+			System.out.println("Shows the count value assigned as: " + count);
+		}
+		
+		//static method
+		static void show() {
+			System.out.println("Shows the static method: "+count );
+		}
+
+		public static void main(String[] args) {
+			
+			System.out.println("Main method static: "+count);
+			
+			
+			//calling a method
+			show();
+			
+			
+		}
+
+}
